543c90b7cc6b6528f7d35ec1ba74bc561348f20a
lorn-aw <sora.x.node@icloud.com>
2021-07-29 18:49:50 +0200

    fix auth controller response

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,7 +3,7 @@ const UserModel = require('../models/user.model');
 module.exports.signUp = async (req, res) => {
     const {pseudo, email, password} = req.body;
     try {
-        const user = UserModel.create({pseudo, email, password});
+        const user = await UserModel.create({pseudo, email, password});
         res.status(201).json({user: user._id})
     } catch (e) {
         res.status(200).send({ e })
